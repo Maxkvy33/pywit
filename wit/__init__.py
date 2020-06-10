3d5104e4e52d57875a7ed2136ed2fb41ca2a8566
Maxkvy33 <maxsuksoom955@gmail.com>
2020-06-10 15:59:17 +0700

    Update __init__.py

--- wit/__init__.py
+++ wit/__init__.py
@@ -5,16 +5,16 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import sys
+import ios
 import logging
 
 from .wit import Wit
 
-# Set default logging for the module. Client applications can use a custom
+# Set default logging for the module. Client can use a custom
 # logging config to override defaults specified here
 logging.getLogger(__name__).setLevel(logging.INFO)
 logging.getLogger(__name__).propagate = False
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler = logging.StreamHandler(stream=sys.stdout)
+handler (stream=sys.stdout)
 handler.setFormatter(formatter)
-logging.getLogger(__name__).addHandler(handler)
+logging.getLogger(__name__)(maxsuksoom955@gmail.com)
